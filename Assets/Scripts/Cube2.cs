df551d04b832405717fdfa45aa4eb0bbb27bef34
Ritik Agarwal <51126956+Ritik210@users.noreply.github.com>
2021-10-30 17:51:52 +0530

    time

---
+++ Assets/Scripts/Cube2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube2 : MonoBehaviour
+{
+    //public GameObject cube;
+    public Material material;
+    MinuteTimer time;
+
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = FindObjectOfType<MinuteTimer>();
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time.timer.text == 1.ToString())
+        {
+            Debug.Log("Working");
+            //material.SetColor("color", Color.blue);
+            rend.material.color = Color.red;
+        }
+        else
+        {
+            rend.material.color = Color.blue;
+        }
+    }
+}
---
+++ Assets/Scripts/Cube4.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cube4 : MonoBehaviour
+{
+    //public GameObject cube;
+    public Material material;
+    MinuteTimer time;
+
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = FindObjectOfType<MinuteTimer>();
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time.timer.text == 3.ToString())
+        {
+            Debug.Log("Working");
+            //material.SetColor("color", Color.blue);
+            rend.material.color = Color.red;
+        }
+        else
+        {
+            rend.material.color = Color.blue;
+        }
+
+        if(time.timer.text == 4.ToString())
+        {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+
+}
